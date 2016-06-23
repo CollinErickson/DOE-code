f2277dfde8136976d8d9e98271aaf95326b74e49
Collin Erickson <collinberickson@gmail.com>
2016-06-23 15:35:33 -0700

    Worked on SLHD, didn't do Algorithm 2
    Alg 2 requires an orthogonal LHD, need to find a way to get that first

--- SLHD.R
+++ SLHD.R
@@ -3,19 +3,35 @@ kronecker.sum.c <- function(D1,D2){
   ((sapply(1:dim(D1)[2],function(i){kronecker.sum(D1[,i],D2[,i])})))
 }
 require('lhs')
-nolhd <- function(m,s,p){#browser()
+snolhd <- function(m,s,p){#browser()
+  # Implements HuangYangLiu2014-ConstructionOfSlicedNearlyOrthogonalLatinHypercubeDesigns
   # p: factors
   # s: slices
   # N = ms
   N <- m*s
-  #EE <- LHD(m,p)
   EE <- simple.LHS(s,p,scaled=F,centered=T)#maximinLHS(n=s,k=p)
-  #FF <- lapply(1:s,function(i)maximinLHS(n=m,k=p,2))
-  FF <- simple.LHS(m,p,scaled=F,centered=T)#maximinLHS(n=m,k=p)
+  #FF <- simple.LHS(m,p,scaled=F,centered=T)#maximinLHS(n=m,k=p)
+  FF <- lapply(1:s,function(ii)simple.LHS(m,p,scaled=F,centered=T))
   #EE <- t(matrix(c(-1,0,1,1,0,-1),nrow=2))
   #FF <- t(matrix(c(1,3,3,-1,5,7,7,-5,-1,-3,-3,1,-5,-7,-7,5)/2,nrow=2))
-  DD <- lapply(1:s,function(i){t(kronecker.sum.c(matrix(EE[i,],nrow=1),s*FF))})
-  DD <- t(matrix(sapply(1:s,function(i){c(t(kronecker.sum.c(matrix(EE[i,],nrow=1),s*FF)))}),nrow=p))
+  #DD <- lapply(1:s,function(i){t(kronecker.sum.c(matrix(EE[i,],nrow=1),s*FF[[i]]))})
+  DD <- t(matrix(sapply(1:s,function(i){c(t(kronecker.sum.c(matrix(EE[i,],nrow=1),s*FF[[i]])))}),nrow=p))
+  
+  # Algorithm 2 makes it (nearly) orthogonal, very hard
+  if (T) {browser()
+    #step 1
+    #try to find orthogonal LHD(m,p)
+    #if fail, stop
+    
+    #step 2
+    #find orthog LHD(m,q)
+    #if not big enough, stop
+    
+    #step 3
+    #obtain s orthogonal LHD(m,p)'s'
+    #if there are s st no two have common rows, stop with output, else stop
+  }
+  
   if(p==2) {#browser()
     require(ggplot2)
     DD.df <- data.frame(DD)
@@ -32,4 +48,4 @@ nolhd <- function(m,s,p){#browser()
 #FF <- t(matrix(c(1,3,3,-1,5,7,7,-5,-1,-3,-3,1,-5,-7,-7,5)/2,nrow=2))
 #kronecker.sum.c(matrix(c(-1,0),ncol=2),3*FF)*2
 
-nolhd(m=8,s=3,p=2)
+snolhd(m=8,s=3,p=2)
