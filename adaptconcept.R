81870cef99ffc2a931431ee0f49fe79b84cddc5e
Collin Erickson <collinberickson@gmail.com>
2016-06-30 08:50:38 -0700

    Started adding tree to adaptconcept, seems to work

--- adaptconcept.R
+++ adaptconcept.R
@@ -1,8 +1,4 @@
-msfunc <- function(func1,xlim,ylim) {#browser()
-  #X1 <- simple.grid(10,2)
-  #X1 <- X1*matrix(c(xlim[2]-xlim[1],ylim[2]-ylim[1]),nrow=nrow(X1),ncol=ncol(X1),byrow=T) + matrix(c(xlim[1],ylim[1]),nrow=nrow(X1),ncol=ncol(X1),byrow=T)
-  #print(summary(X1))
-  #print(summary(simple.grid(10,2,scaledto=rbind(xlim,ylim))))
+msfunc <- function(func1,xlim,ylim) {
   X1 <- simple.grid(10,2,scaledto=rbind(xlim,ylim))
   mean(apply(X1,1,func1)^2)
 }
@@ -44,8 +40,26 @@ adapt.concept <- function() {
 require(mlegp)
 require(contourfilled)
 source('LHS.R')
-adapt.concept2 <- function(func,g=3,level=0,xlim=c(0,1),ylim=c(0,1),X=NULL,Z=NULL,maxmse.levelup=-Inf,xlim.second=NULL,ylim.second=NULL) {browser()
+# To do
+# Get tree working
+# secondmax all the way up
+adapt.concept2 <- function(func,g=3,level=0,xlim=c(0,1),ylim=c(0,1),X=NULL,Z=NULL,maxmse.levelup=-Inf,xlim.second=NULL,ylim.second=NULL,adapt.tree=NULL) {browser()
   print(paste('At level',level))
+  
+  # trying tree stuff
+  if(is.null(adapt.tree)) { # creates root
+    adapt.tree <- Node$new('root')
+    adapt.tree$lev <- level # should always be zero here
+    outer(1:g,1:g,Vectorize(function(a,b){adapt.tree$AddChild(paste(a,b))}))
+    adapt.tree$samples <- 1
+  } else if(is.null(adapt.tree$samples)) { # if not visited yet, initialize
+    adapt.tree$lev <- level
+    outer(1:g,1:g,Vectorize(function(a,b){adapt.tree$AddChild(paste(a,b))}))
+    adapt.tree$samples <- 1
+  } else { # else already visited, increment samples
+    adapt.tree$samples <- adapt.tree$samples + 1
+  }
+  
   #if (not been here already) {
   #get sample
   Xnew <- simple.grid(g,2,scaledto=rbind(xlim,ylim))
@@ -93,15 +107,20 @@ adapt.concept2 <- function(func,g=3,level=0,xlim=c(0,1),ylim=c(0,1),X=NULL,Z=NUL
       ylim.next <- ylim[1]+(ylim[2]-ylim[1])*c(maxmse.ind[2]-1,maxmse.ind[2])/g
       xlim.nextsecond <- xlim[1]+(xlim[2]-xlim[1])*c(secondmaxmse.ind[1]-1,secondmaxmse.ind[1])/g
       ylim.nextsecond <- ylim[1]+(ylim[2]-ylim[1])*c(secondmaxmse.ind[2]-1,secondmaxmse.ind[2])/g
-      rect(xlim.next[1],ylim.next[1],xlim.next[2],ylim.next[2],lwd=5,border='gray')
-      #rect(xlim.next[1],ylim.next[1],xlim.next[2],ylim.next[2],col=rgb(1,1,0,.3))
+      rect(xlim.next[1],ylim.next[1],xlim.next[2],ylim.next[2],lwd=5,border='red')
+      rect(xlim.nextsecond[1],ylim.nextsecond[1],xlim.nextsecond[2],ylim.nextsecond[2],lwd=2,border='black')
       rect(xlim.next[1],ylim.next[1],xlim.next[2],ylim.next[2],col=1,angle=45,density=6+2*level^2)
       print(paste('Diving to xlim, ylim:',xlim.next[1],xlim.next[2],ylim.next[1],ylim.next[2],collapse = ''))
+      
+      # RECURSIVE STEP HERE
       ac.out <- adapt.concept2(func=func,g=g,level=level+1, 
                      xlim=xlim.next, 
                      ylim=ylim.next, 
                      X=X,Z=Z,maxmse.levelup=secondmaxmse,
-                     xlim.second=xlim.nextsecond,ylim.second=ylim.nextsecond)
+                     xlim.second=xlim.nextsecond,ylim.second=ylim.nextsecond,
+                     adapt.tree = adapt.tree$children[[paste(maxmse.ind,collapse = ' ')]]
+                     )
+      
       print(paste('Back at level',level))
       X <- ac.out$X
       Z <- ac.out$Z
@@ -112,7 +131,8 @@ adapt.concept2 <- function(func,g=3,level=0,xlim=c(0,1),ylim=c(0,1),X=NULL,Z=NUL
   }
 }
 if (F) {
-  adapt.concept2(function(xx) exp(-sum((xx-.5)^2)/2/.1),g=2)
+  gaussian <- function(xx) exp(-sum((xx-.5)^2)/2/.1)
+  adapt.concept2(function(xx) exp(-sum((xx-.5)^2)/2/.1),g=3)
   banana <- function(xx){exp(-.5*(xx[1]*80-40)^2/100-.5*((xx[2]*30-20)+.03*(xx[1]*80-40)^2-3)^2)}
   contourfilled.func(banana)
   adapt.concept2(banana)
