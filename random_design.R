e521b4ea68a968be8d32bd491c1763a346677b0a
Collin Erickson <collinberickson@gmail.com>
2017-11-15 16:35:53 -0600

    Fixed Sobol seed, needs scrambling

--- random_design.R
+++ random_design.R
@@ -47,7 +47,7 @@ sobol_design <- R6::R6Class("sobol_design",
                                  self$L <- L
                                  self$seed <- seed
                                  # D=1 sobol gives vector, as.matrix makes it matrix with 1 col
-                                 self$X <- as.matrix(randtoolbox::sobol(n=L,dim=D, init=T, seed=seed))
+                                 self$X <- as.matrix(randtoolbox::sobol(n=L,dim=D, init=T, seed=seed, scrambling=1))
                                },
                                get.batch = function(L=self$L) {
                                  # if (length(self$seed) > 0) {
@@ -55,7 +55,7 @@ sobol_design <- R6::R6Class("sobol_design",
                                  #   self$seed <- self$seed + 1
                                  # }
                                  # D=1 sobol gives vector, as.matrix makes it matrix with 1 col
-                                 newX <- as.matrix(randtoolbox::sobol(n=L, dim=self$D, init=F))
+                                 newX <- as.matrix(randtoolbox::sobol(n=L, dim=self$D, init=F, scrambling=1))
                                  self$X <- rbind(self$X, newX)
                                  newX
                                }
