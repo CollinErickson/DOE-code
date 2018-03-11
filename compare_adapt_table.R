cfde6077d5a9868e73cec239ea5a94e890694ca9
Collin Erickson <collinberickson@gmail.com>
2018-03-10 23:05:54 -0500

    Working on getting table in latex for paper

---
+++ compare_adapt_table.R
@@ -0,0 +1,40 @@
+get_table <- function(self) {#browser()
+  #  "Group", 
+  tab <- self$endmeandf[, c("obj","design","actual_intwerror_Q1", "actual_intwerror", "actual_intwerror_Q3")]
+  colnames(tab) <- c("Selection", "Candidates","$\\Psi(25\\%)$", "$\\Psi(50\\%)$", "$\\Psi(75\\%)$")
+  tab$Selection[tab$Selection == "desirability"] <- "Proposed"
+  tab$Selection[tab$Selection == "nonadapt"] <- "In order"
+  tab$Candidates[tab$Candidates == "sobol"] <- "Sobol"
+  tab
+}
+gt <- get_table(ban1)
+round_df <- function(df, digits) {
+  nums <- vapply(df, is.numeric, FUN.VALUE = logical(1))
+  
+  df[,nums] <- round(df[,nums], digits = digits)
+  
+  (df)
+}
+# xtable::xtable(gt)
+get_xtable <- function(self, caption=NULL, label=NULL) {
+  
+  gt <- get_table(self)
+  # browser()
+  gt2 <- round_df(gt, digits=3)
+  
+  # Bold smallest
+  bold_small_columns <- 3:5
+  for (i in bold_small_columns) {
+    ti <- which.min(gt[,i])
+    gt2[ti,i] <- paste0("\\textbf{", gt2[ti,i],"}")
+  }
+  
+  kab <- xtable::xtable(gt2, digits=3, caption=caption, label=label)
+  kab
+}
+
+print(get_xtable(ban1, caption="Comparison of methods on the Banana function",
+                 label="bananatable"),
+      sanitize.text.function=identity, digits=4,
+      include.rownames=FALSE
+      )
