59a7a528af22752c5ad001b19a6ac3f3721d9f68
Collin Erickson <collinberickson@gmail.com>
2018-03-18 18:12:50 -0500

    Updated table

--- compare_adapt_table.R
+++ compare_adapt_table.R
@@ -1,13 +1,14 @@
 get_table <- function(self) {#browser()
   #  "Group", 
-  tab <- self$endmeandf[, c("obj","design","actual_intwerror_Q1", "actual_intwerror", "actual_intwerror_Q3")]
+  tab <- self$endmeandf[, c("selection_method","design","actual_intwerror_Q1", "actual_intwerror", "actual_intwerror_Q3")]
   colnames(tab) <- c("Selection", "Candidates","$\\Psi(25\\%)$", "$\\Psi(50\\%)$", "$\\Psi(75\\%)$")
-  tab$Selection[tab$Selection == "desirability"] <- "Proposed"
+  # tab$Selection[tab$Selection == "desirability"] <- "Proposed"
+  tab$Selection[tab$Selection == "max_des_red_all_best"] <- "Proposed"
   tab$Selection[tab$Selection == "nonadapt"] <- "In order"
   tab$Candidates[tab$Candidates == "sobol"] <- "Sobol"
   tab
 }
-gt <- get_table(ban1)
+# gt <- get_table(ban1)
 round_df <- function(df, digits) {
   nums <- vapply(df, is.numeric, FUN.VALUE = logical(1))
   
@@ -33,8 +34,10 @@ get_xtable <- function(self, caption=NULL, label=NULL) {
   kab
 }
 
-print(get_xtable(ban1, caption="Comparison of methods on the Banana function",
-                 label="bananatable"),
-      sanitize.text.function=identity, digits=4,
-      include.rownames=FALSE
-      )
+if (F) {
+  print(get_xtable(ban1, caption="Comparison of methods on the Banana function",
+                   label="bananatable"),
+        sanitize.text.function=identity, digits=4,
+        include.rownames=FALSE
+        )
+}
