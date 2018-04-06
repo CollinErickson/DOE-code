2d40ee3a58dcf8f80f350275413aa8c08df26f49
Collin Erickson <collinberickson@gmail.com>
2018-04-06 15:48:40 -0500

    Changing to mean (se)

--- compare_adapt_table2.R
+++ compare_adapt_table2.R
@@ -53,14 +53,17 @@ get_xtable <- function(self1, self2, self3, caption=NULL, label=NULL, digits=3, 
   g3[,3:4] <- g3[,3:4]*1e2
   # Round to specified digits
   # gt2 <- round_df(gt, digits=digits, rnd=rnd)
-  gr1 <- round_df(g1, digits=digits, rnd=rnd)
-  gr2 <- round_df(g2, digits=digits, rnd=rnd)
-  gr3 <- round_df(g3, digits=digits, rnd=rnd)
+  gr1 <- g1 #round_df(g1, digits=digits, rnd=rnd)
+  gr2 <- g2 #round_df(g2, digits=digits, rnd=rnd)
+  gr3 <- g3 #round_df(g3, digits=digits, rnd=rnd)
   # Paste together mean+-sd
-  sepstr <- " $\\pm$ "
-  gr1$meanpmsd <- paste(gr1[,3],gr1[,4],sep=sepstr)
-  gr2$meanpmsd <- paste(gr2[,3],gr2[,4],sep=sepstr)
-  gr3$meanpmsd <- paste(gr3[,3],gr3[,4],sep=sepstr)
+  # sepstr <- " $\\pm$ "
+  # To put se in parentheses
+  sepstr <- " ("
+  endstr <- ")"
+  gr1$meanpmsd <- paste0(sprintf("%.2f",gr1[,3]),sepstr,sprintf("%.2f",gr1[,4]),endstr)
+  gr2$meanpmsd <- paste0(sprintf("%.2f",gr2[,3]),sepstr,sprintf("%.2f",gr2[,4]),endstr)
+  gr3$meanpmsd <- paste0(sprintf("%.2f",gr3[,3]),sepstr,sprintf("%.2f",gr3[,4]),endstr)
   # browser()
   # gt[,3] <- gt[,3]/1e6
   # gt[,4] <- gt[,4]*1e3
@@ -77,12 +80,26 @@ get_xtable <- function(self1, self2, self3, caption=NULL, label=NULL, digits=3, 
   #   gt2[ti,i] <- paste0("\\textbf{", gt2[ti,i],"}")
   # }
   # browser()
+  # Bold only best in each row
   wm1 <- which.min(g1[,3])
-  gt[wm1,3] <- paste0("\\textbf{", paste(gr1[wm1,3],gr1[wm1,4],sep=sepstr),"}")
+  # # gt[wm1,3] <- paste0("\\textbf{", paste0(gr1[wm1,3],sepstr,gr1[wm1,4],endstr),"}")
+  # gt[wm1,3] <- paste0("\\textbf{", gt[wm1,3],"}")
   wm2 <- which.min(g2[,3])
-  gt[wm2,4] <- paste0("\\textbf{", paste(gr2[wm2,3],gr2[wm2,4],sep=sepstr),"}")
+  # gt[wm2,4] <- paste0("\\textbf{", gt[wm2,4],"}")
   wm3 <- which.min(g3[,3])
-  gt[wm3,5] <- paste0("\\textbf{", paste(gr3[wm3,3],gr3[wm3,4],sep=sepstr),"}")
+  # gt[wm3,5] <- paste0("\\textbf{", gt[wm3,5],"}")
+  
+  # browser()
+  # Bold all not significantly different from minimum
+  pvalues1 <- sapply(1:nrow(g1), function(i) {pnorm(-abs(((g1[wm1,3])-(g1[i,3])) / sqrt((g1[wm1,4]^2)+(g1[i,4]^2)))) * 2})
+  boldinds1 <- pvalues1 > 0.05
+  gt[boldinds1 > 0.05, 3] <- paste0("\\textbf{", gt[boldinds1,3],"}")
+  pvalues2 <- sapply(1:nrow(g2), function(i) {pnorm(-abs(((g2[wm2,3])-(g2[i,3])) / sqrt((g2[wm2,4]^2)+(g2[i,4]^2)))) * 2})
+  boldinds2 <- pvalues2 > 0.05
+  gt[boldinds2 > 0.05, 4] <- paste0("\\textbf{", gt[boldinds2,4],"}")
+  pvalues3 <- sapply(1:nrow(g3), function(i) {pnorm(-abs(((g3[wm3,3])-(g3[i,3])) / sqrt((g3[wm3,4]^2)+(g3[i,4]^2)))) * 2})
+  boldinds3 <- pvalues3 > 0.05
+  gt[boldinds3 > 0.05, 5] <- paste0("\\textbf{", gt[boldinds3,5],"}")
   
   if (no_candidates) { # Don't print candidates column
     gt <- gt[,-2]
@@ -108,6 +125,8 @@ if (F) {
   )
   tp2 <- paste(tp1, collapse='\n')
   stringi::stri_sub(tp2, regexpr('\\hline', tp2)[1]-1, regexpr('\\hline', tp2)[1]-2
-                    ) <- paste0("\\hline\n & \\multicolumn{3}{c}{Average $\\Phi$ from ",bran1$reps," replicates} \\\\\n")
+                    ) <- paste0("\\hline\n & \\multicolumn{3}{c}{",
+                                "Mean $\\Phi$ (std. error $\\Phi$) from ",
+                                bran1$reps," replicates} \\\\\n")
   cat(tp2)
 }
