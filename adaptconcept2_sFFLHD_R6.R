c14b7ae0fa82654badef4679b9011e448f21cdf7
Collin Erickson <collinberickson@gmail.com>
2018-04-08 22:13:55 -0500

    Small changes

--- adaptconcept2_sFFLHD_R6.R
+++ adaptconcept2_sFFLHD_R6.R
@@ -1571,16 +1571,24 @@ if (F) {
     selection_method="max_des_red_all_best"); a$run(1)
   
   # limnonpoly, grad_norm2_mean, laGP_GauPro_kernel
-  set.seed(2); csa(); a <- adapt.concept2.sFFLHD.R6$new(
-    D=2,L=3,func=limnonpoly,
+  set.seed(1); csa(); a <- adapt.concept2.sFFLHD.R6$new(
+    D=2,L=3,func=limnonpoly, nugget = 1e-7,estimate.nugget = F,
     obj="desirability", des_func=des_func_grad_norm2_mean,
     actual_des_func=NULL,#get_num_actual_des_func_grad_norm2_mean(),
     stage1batches=2, alpha_des=1, weight_const=0,
     package="laGP_GauPro_kernel", design='sFFLHD',
-    selection_method="max_des_red_all_best"); a$run(1)
+    selection_method="max_des_red_all_best"
+    # selection_method="ALC_all_best"
+    ); a$run(1)
   a$run(14)
+  # Show predicted mean and batch when pts selected
   cf(a$mod$predict, batchmax=Inf,
      afterplotfunc=function() {
        text(a$X[,1], a$X[,2], a$X_tracker$iteration_added)},
      xlim=c(-.02,1.02), ylim=c(-.02,1.02), bar=T)
+  # Show true function and batch when pts selected
+  cf(limnonpoly, batchmax=Inf,
+     afterplotfunc=function() {
+       text(a$X[,1], a$X[,2], a$X_tracker$iteration_added)},
+     xlim=c(-.02,1.02), ylim=c(-.02,1.02), bar=F, mainminmax=F)
 }
