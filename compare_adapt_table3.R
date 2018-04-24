04ab7cebda3f555575cdedd8a04d44b90817f354
Collin Erickson <collinberickson@gmail.com>
2018-04-24 17:01:36 -0500

    Removing previous code to clean up

--- compare_adapt_table3.R
+++ compare_adapt_table3.R
@@ -48,36 +48,15 @@ get_xtable <- function(selfs, nams, caption=NULL, label=NULL, digits=3, rnd=T, n
   
   # Get tables
   gs <- lapply(selfs, get_table)
-  # g1 <- get_table(self1)
-  # g2 <- get_table(self2)
-  # g3 <- get_table(self3)
-  # g4 <- get_table(self4)
-  # g5 <- get_table(self5)
-  # g6 <- get_table(self6)
-  # g7 <- get_table(self7)
   # Rescale mean and sd
   scalebys <- floor(sapply(gs, function(gi) {log(min(gi[,3]), 10)}))
   gs <- mapply(gs, scalebys, FUN=function(gi, si) {
     gi[,3:4] <- gi[,3:4]/(10^si)
     gi
   }, SIMPLIFY = FALSE)
-  # g1[,3:4] <- g1[,3:4]/1e7
-  # g2[,3:4] <- g2[,3:4]*1e3
-  # g3[,3:4] <- g3[,3:4]*1e-1
-  # g4[,3:4] <- g4[,3:4]*1e9
-  # g5[,3:4] <- g5[,3:4]*1e3
-  # g6[,3:4] <- g6[,3:4]*1e4
-  # g7[,3:4] <- g7[,3:4]*1e2
   # Round to specified digits
   # gt2 <- round_df(gt, digits=digits, rnd=rnd)
   grs <- gs
-  # gr1 <- g1 #round_df(g1, digits=digits, rnd=rnd)
-  # gr2 <- g2 #round_df(g2, digits=digits, rnd=rnd)
-  # gr3 <- g3 #round_df(g3, digits=digits, rnd=rnd)
-  # gr4 <- g4
-  # gr5 <- g5
-  # gr6 <- g6
-  # gr7 <- g7
   # Paste together mean+-sd
   # sepstr <- " $\\pm$ "
   # To put se in parentheses
@@ -88,19 +67,6 @@ get_xtable <- function(selfs, nams, caption=NULL, label=NULL, digits=3, rnd=T, n
     gri$meanpmsd <- paste0(sprintf("%.2f",gri[,3]),sepstr,sprintf("%.2f",gri[,4]),endstr)
     gri
   })
-  # gr1$meanpmsd <- paste0(sprintf("%.2f",gr1[,3]),sepstr,sprintf("%.2f",gr1[,4]),endstr)
-  # gr2$meanpmsd <- paste0(sprintf("%.2f",gr2[,3]),sepstr,sprintf("%.2f",gr2[,4]),endstr)
-  # gr3$meanpmsd <- paste0(sprintf("%.2f",gr3[,3]),sepstr,sprintf("%.2f",gr3[,4]),endstr)
-  # gr4$meanpmsd <- paste0(sprintf("%.2f",gr4[,3]),sepstr,sprintf("%.2f",gr4[,4]),endstr)
-  # gr5$meanpmsd <- paste0(sprintf("%.2f",gr5[,3]),sepstr,sprintf("%.2f",gr5[,4]),endstr)
-  # gr6$meanpmsd <- paste0(sprintf("%.2f",gr6[,3]),sepstr,sprintf("%.2f",gr6[,4]),endstr)
-  # gr7$meanpmsd <- paste0(sprintf("%.2f",gr7[,3]),sepstr,sprintf("%.2f",gr7[,4]),endstr)
-  # browser()
-  # gt[,3] <- gt[,3]/1e6
-  # gt[,4] <- gt[,4]*1e3
-  # gt[,5] <- gt[,5]*1e3
-  # gt <- cbind(g1[,1:3], g2[,3], g3[,3])
-  # gt <- cbind(gr1[,c(1:2,5)], gr2[,5], gr3[,5], gr4[,5], gr5[,5], gr6[,5], gr7[,5], stringsAsFactors=F)
   
   gt <- do.call(cbind, list(grs[[1]][,c(1:2)], lapply(grs, function(gri) {gri[,5]}), stringsAsFactors=F))
   # browser()
@@ -112,27 +78,9 @@ get_xtable <- function(selfs, nams, caption=NULL, label=NULL, digits=3, rnd=T, n
   # c('Branin ($\\times 10^{-6}$)', 'Franke ($\\times 10^{3}$)', 'Lim ($\\times 10^{2}$)', '4','5','6','7')
   
   # Bold smallest
-  # bold_small_columns <- 3:5
-  # for (i in bold_small_columns) {
-  #   ti <- which.min(gt[,i])
-  #   gt2[ti,i] <- paste0("\\textbf{", gt2[ti,i],"}")
-  # }
-  # browser()
   # Bold only best in each row
   wms <- sapply(gs, function(gi) {which.min(gi[,3])})
-  # wm1 <- which.min(g1[,3])
-  # # # gt[wm1,3] <- paste0("\\textbf{", paste0(gr1[wm1,3],sepstr,gr1[wm1,4],endstr),"}")
-  # # gt[wm1,3] <- paste0("\\textbf{", gt[wm1,3],"}")
-  # wm2 <- which.min(g2[,3])
-  # # gt[wm2,4] <- paste0("\\textbf{", gt[wm2,4],"}")
-  # wm3 <- which.min(g3[,3])
-  # wm4 <- which.min(g4[,3])
-  # wm5 <- which.min(g5[,3])
-  # wm6 <- which.min(g6[,3])
-  # wm7 <- which.min(g7[,3])
-  # # gt[wm3,5] <- paste0("\\textbf{", gt[wm3,5],"}")
   
-  # browser()
   # Bold all not significantly different from minimum
   pvalues <- mapply(gs, wms,
                     FUN=function(gi, wmi) {
@@ -145,34 +93,11 @@ get_xtable <- function(selfs, nams, caption=NULL, label=NULL, digits=3, rnd=T, n
   for(i in 1:(ncol(boldinds))) {
     gt[boldinds[,i]>0.05, 2+i] <- paste0("\\textbf{", gt[boldinds[,i],2+i],"}")
   }
-
-  # pvalues1 <- sapply(1:nrow(g1), function(i) {pnorm(-abs(((g1[wm1,3])-(g1[i,3])) / sqrt((g1[wm1,4]^2)+(g1[i,4]^2)))) * 2})
-  # boldinds1 <- pvalues1 > 0.05
-  # gt[boldinds1 > 0.05, 3] <- paste0("\\textbf{", gt[boldinds1,3],"}")
-  # pvalues2 <- sapply(1:nrow(g2), function(i) {pnorm(-abs(((g2[wm2,3])-(g2[i,3])) / sqrt((g2[wm2,4]^2)+(g2[i,4]^2)))) * 2})
-  # boldinds2 <- pvalues2 > 0.05
-  # gt[boldinds2 > 0.05, 4] <- paste0("\\textbf{", gt[boldinds2,4],"}")
-  # pvalues3 <- sapply(1:nrow(g3), function(i) {pnorm(-abs(((g3[wm3,3])-(g3[i,3])) / sqrt((g3[wm3,4]^2)+(g3[i,4]^2)))) * 2})
-  # boldinds3 <- pvalues3 > 0.05
-  # gt[boldinds3 > 0.05, 5] <- paste0("\\textbf{", gt[boldinds3,5],"}")
-  # pvalues4 <- sapply(1:nrow(g4), function(i) {pnorm(-abs(((g4[wm4,3])-(g4[i,3])) / sqrt((g4[wm4,4]^2)+(g4[i,4]^2)))) * 2})
-  # boldinds4 <- pvalues4 > 0.05
-  # gt[boldinds4 > 0.05, 6] <- paste0("\\textbf{", gt[boldinds4,6],"}")
-  # pvalues5 <- sapply(1:nrow(g5), function(i) {pnorm(-abs(((g5[wm5,3])-(g5[i,3])) / sqrt((g5[wm5,4]^2)+(g5[i,4]^2)))) * 2})
-  # boldinds5 <- pvalues5 > 0.05
-  # gt[boldinds5 > 0.05, 7] <- paste0("\\textbf{", gt[boldinds5,7],"}")
-  # pvalues6 <- sapply(1:nrow(g6), function(i) {pnorm(-abs(((g6[wm6,3])-(g6[i,3])) / sqrt((g6[wm6,4]^2)+(g6[i,4]^2)))) * 2})
-  # boldinds6 <- pvalues6 > 0.05
-  # gt[boldinds6 > 0.05, 8] <- paste0("\\textbf{", gt[boldinds6,8],"}")
-  # pvalues7 <- sapply(1:nrow(g7), function(i) {pnorm(-abs(((g7[wm7,3])-(g7[i,3])) / sqrt((g7[wm7,4]^2)+(g7[i,4]^2)))) * 2})
-  # boldinds7 <- pvalues7 > 0.05
-  # gt[boldinds7 > 0.05, 9] <- paste0("\\textbf{", gt[boldinds7,9],"}")
   
   if (no_candidates) { # Don't print candidates column
     gt <- gt[,-2]
   }
   kab <- xtable::xtable(gt, caption=caption, label=label, align="llrrrrrrr")
-  # browser()
   kab
 }
 
