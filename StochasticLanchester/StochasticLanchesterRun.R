0b66425696f34550a284dce86ae9afae2ea31d8c
Collin Erickson <collinberickson@gmail.com>
2018-10-16 20:23:18 -0500

    Getting better stats for StochLanch

--- StochasticLanchester/StochasticLanchesterRun.R
+++ StochasticLanchester/StochasticLanchesterRun.R
@@ -57,6 +57,7 @@ if (F) {
   cf_highdim(a$mod$mod.extra$GauPro$mod$grad_norm2_mean, D=4, pts=a$X,
              average=T, average_reps=1e3, batchmax = Inf)
   dev.off()
+  write.csv(x = a$X, file = ".//StochasticLanchester//PointsSelectedInPaperDemo.csv")
 }
 
 if (F) {
@@ -67,28 +68,49 @@ if (F) {
   actual <- datadf[,17]
   plot(preds, actual); abline(a=0,b=1, col=2)
   cor(preds, actual)
-  1 - sum((preds-actual)^2) / sum((actual-mean(actual))^2)
+  R2_1 <- 1 - sum((preds-actual)^2) / sum((actual-mean(actual))^2)
+  R2_1
   
   d2 <- datadf[datadf[,3]<.5,]
   preds2 <- a$mod$predict(as.matrix(d2[,1:4]))
   actual2 <- d2[,17]
   plot(preds2, actual2); abline(a=0,b=1, col=2)
   cor(preds2, actual2)
-  1 - sum((preds2-actual2)^2) / sum((actual2-mean(actual2))^2)
+  R2_2 <- 1 - sum((preds2-actual2)^2) / sum((actual2-mean(actual2))^2)
+  R2_2
   
   d3 <- datadf[datadf[,3]>.5,]
   preds3 <- a$mod$predict(as.matrix(d3[,1:4]))
   actual3 <- d2[,17]
   plot(preds3, actual3); abline(a=0,b=1, col=2)
   cor(preds3, actual3)
-  1 - sum((preds3-actual3)^2) / sum((actual3-mean(actual3))^2)
-  
+  R2_3 <- 1 - sum((preds3-actual3)^2) / sum((actual3-mean(actual3))^2)
+  R2_3
 }
 
 if (F) {
   # Run comparison samples
-  runone <- function() {
+  runone <- function() {#browser()
     rowsinds <- sample(1:nrow(datadf), 48, replace = F)
-    X <- 
+    datadfi <- datadf[rowsinds, ]
+    mod <- IGP::IGP(X=as.matrix(datadfi[,1:4]), Z=datadfi[,17], package='laGP_GauPro_kernel')
+    preds <- mod$predict(as.matrix(datadf[,1:4]))
+    actual <- datadf[,17]
+    #plot(preds, actual); abline(a=0,b=1, col=2)
+    cor(preds, actual)
+    R2 <- 1 - sum((preds-actual)^2) / sum((actual-mean(actual))^2)
+    R2_x3p5 <- 1 - sum((preds[datadfi[,3]<.5]-actual[datadfi[,3]<.5])^2) / sum((actual[datadfi[,3]<.5]-mean(actual[datadfi[,3]<.5]))^2)
+    R2_x3p25 <- 1 - sum((preds[datadfi[,3]<.25]-actual[datadfi[,3]<.25])^2) / sum((actual[datadfi[,3]<.25]-mean(actual[datadfi[,3]<.25]))^2)
+    R2_x3p5x4p5 <- 1 - sum((preds[datadfi[,3]<.5 & datadfi[,4]<.5]-actual[datadfi[,3]<.5 & datadfi[,4]<.5])^2) / 
+      sum((actual[datadfi[,3]<.5 & datadfi[,4]<.5]-mean(actual[datadfi[,3]<.5 & datadfi[,4]<.5]))^2)
+    c("R2"=R2, "R2_x3p5"=R2_x3p5, "R2_x3p25"=R2_x3p25, "R2_x3p5x4p5"=R2_x3p5x4p5)
   }
+  runone()
+  R2s <- plyr::rdply(100, runone())
+  table(R2_1 > R2s$R2)
+  table(R2_2 > R2s$R2_x3p5)
+  hist(R2s$R2, breaks = 40); abline(v=1 - sum((preds-actual)^2) / sum((actual-mean(actual))^2), col=2)
+  hist(R2s$R2_x3p5, breaks = 40); abline(v=1 - sum((preds2-actual2)^2) / sum((actual2-mean(actual2))^2), col=2)
+  ggplot(R2s, aes(x=R2)) + geom_histogram(bins=30) + geom_vline(xintercept = R2_1, color="red")
+  ggplot(R2s, aes(x=R2_x3p5)) + geom_histogram(bins=30) + geom_vline(xintercept = R2_2, color="red")
 }
