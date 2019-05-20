5bfe8a6eb7e60b24760a494feff5d41b11354e37
Collin Erickson <collinberickson@gmail.com>
2019-05-20 16:51:01 -0500

    Setting up adapt paper runs 4 to run on Crunch

--- adapt_paper_runs4_addtruegrad.R
+++ adapt_paper_runs4_addtruegrad.R
@@ -27,6 +27,7 @@ des_funcs <- c('des_func_grad_norm2_mean','des_func_grad_norm2_mean',
 source('.//compare_adaptconceptR6.R')
 
 run_test <- function(funcstring, reps, batches, D, L, stage1batches,
+                     use_parallel=TRUE,
                      seed_start, design_seed_start, startover=FALSE) {
   # print("c1 doesn't exist, creating new")
   if (Sys.info()['sysname'] == "Windows") {
@@ -35,8 +36,10 @@ run_test <- function(funcstring, reps, batches, D, L, stage1batches,
     which_matches <- which(substr(commandArgs(),1,18) == "number_of_threads=")
     if (length(which_matches) == 1) {
       parallel_cores <- as.integer(substr(commandArgs()[which_matches], 19, 21))
+      cat("Found number of cores = ", parallel_cores, '\n')
     } else {
       parallel_cores <- 1
+      cat("Didn't find number of cores to use, setting to 1\n")
     }
   }
   c1 <- compare.adaptR6$new(func=funcstring, reps=reps, batches=batches, D=D, L=L,
@@ -46,13 +49,13 @@ run_test <- function(funcstring, reps, batches, D, L, stage1batches,
                             design=c(designs, "sFFLHD_Lflex"),
                             des_func=c(des_funcs, 
                                        paste0('actual_des_func_grad_norm2_mean_', funcstring)
-                                       ), # HERE is key, add true one
+                            ), # HERE is key, add true one
                             actual_des_func=paste0('actual_des_func_grad_norm2_mean_', funcstring),
                             alpha_des=1, weight_const=0,
                             package="laGP_GauPro_kernel",
                             error_power=2,
                             seed_start=seed_start, design_seed_start=design_seed_start,
-                            parallel=T, # always do parallel for temp_save
+                            parallel=use_parallel, # always do parallel for temp_save
                             parallel_cores=parallel_cores, save_output=FALSE
   )
   # Check if already saved
@@ -68,7 +71,17 @@ run_test <- function(funcstring, reps, batches, D, L, stage1batches,
   # Run all, save temps
   print("Running all c1")
   already_run <- sum(c1$completed_runs)
-  c1$run_all(parallel_temp_save=TRUE, noplot=TRUE, run_order="reverse")
+  if (use_parallel) {
+    c1$run_one(parallel_temp_save=TRUE, noplot=TRUE, run_order="reverse")
+  } else {
+    # For not parallel
+    while (TRUE) {
+      try(c1$run_one(noplot=TRUE))
+      c1$save_self()
+      print(table(c1$completed_runs))
+      if (all(c1$completed_runs == TRUE)) {break}
+    }
+  }
   print("Finished c1, saving")
   if (sum(c1$completed_runs) > already_run) {c1$save_self()}
   return(c1)
@@ -97,14 +110,42 @@ if (F) {
                           stage1batches=3, seed_start=1004000, design_seed_start=1014000))
   otl1    <- try(run_test(funcstring='OTL_Circuit', D=6,  L=4, batches=4*15, reps=reps,
                           stage1batches=4, seed_start=1005000, design_seed_start=1015000))
-  piston1 <- try(run_test(funcstring='piston',      D=7,  L=5, batches=4*14 / 2, reps=reps,
+  piston1 <- try(run_test(funcstring='piston',      D=7,  L=5, batches=4*14, reps=reps,
                           stage1batches=4, seed_start=1006000, design_seed_start=1016000));print("cut batches in half")
   bh1     <- try(run_test(funcstring='borehole',    D=8,  L=5, batches=4*16, reps=reps,
                           stage1batches=5, seed_start=1007000, design_seed_start=1017000))
   wing1   <- try(run_test(funcstring='wingweight',  D=10, L=5, batches=4*20, reps=reps,
                           stage1batches=6, seed_start=1008000, design_seed_start=1018000))
   
+}
+
+# Look at results
+if (F) {
   bran1$outrawdf$Method <- Group.names.clean[bran1$outrawdf$Group]
   ggplot(data=bran1$outrawdf, mapping=aes(n, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10()
   ggplot(data=bran1$outrawdf %>% filter(n %in% c(20,40)), mapping=aes(des_func, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10() + facet_wrap(. ~ n)
+  ggplot(data=bran1$outrawdf %>% filter(n %in% c(20,40)), mapping=aes(Method, actual_intwerror, color=des_func)) + geom_point(size=5) + scale_y_log10() + facet_wrap(. ~ n)
+  
+  franke1$outrawdf$Method <- Group.names.clean[franke1$outrawdf$Group]
+  ggplot(data=franke1$outrawdf, mapping=aes(n, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10()
+  ggplot(data=franke1$outrawdf %>% filter(n %in% c(20,40)), mapping=aes(des_func, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10() + facet_wrap(. ~ n)
+  ggplot(data=franke1$outrawdf %>% filter(n %in% c(20,40)), mapping=aes(Method, actual_intwerror, color=des_func)) + geom_point(size=5) + scale_y_log10() + facet_wrap(. ~ n)
+  ggplot(data=franke1$outrawdf %>% filter(n %in% c(20,40)), mapping=aes(Method, actual_intwerrorquants.5, color=des_func)) + geom_point(size=5) + scale_y_log10() + facet_wrap(. ~ n)
+  
+  lim1$outrawdf$Method <- Group.names.clean[lim1$outrawdf$Group]
+  ggplot(data=lim1$outrawdf, mapping=aes(n, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10()
+  ggplot(data=lim1$outrawdf %>% filter(n %in% c(20,40)), mapping=aes(des_func, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10() + facet_wrap(. ~ n)
+  ggplot(data=lim1$outrawdf %>% filter(n %in% c(20,40)), mapping=aes(Method, actual_intwerror, color=des_func)) + geom_point(size=5) + scale_y_log10() + facet_wrap(. ~ n)
+  
+  beam1$outrawdf$Method <- Group.names.clean[beam1$outrawdf$Group]
+  ggplot(data=beam1$outrawdf, mapping=aes(n, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10()
+  ggplot(data=beam1$outrawdf %>% filter(n %in% c(30,60)), mapping=aes(des_func, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10() + facet_wrap(. ~ n)
+  ggplot(data=beam1$outrawdf %>% filter(n %in% c(30,60)), mapping=aes(Method, actual_intwerror, color=des_func)) + geom_point(size=5) + scale_y_log10() + facet_wrap(. ~ n)
+  
+  wing1 <- readRDS("./compare_adaptconcept_output/wingweight_D=10_L=5_b=5_B=40_R=10_n0=0_s1b=6_S=1008000/object.rds")
+  wing1$outrawdf$Method <- Group.names.clean[wing1$outrawdf$Group]
+  ggplot(data=wing1$outrawdf, mapping=aes(n, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10()
+  ggplot(data=wing1$outrawdf %>% filter(n %in% c(100,200)), mapping=aes(des_func, actual_intwerror, color=des_func)) + geom_point() + scale_y_log10() + facet_wrap(. ~ n)
+  ggplot(data=wing1$outrawdf %>% filter(n %in% c(100,200)), mapping=aes(Method, actual_intwerror, color=des_func)) + geom_point(size=5) + scale_y_log10() + facet_wrap(. ~ n)
+  
 }
