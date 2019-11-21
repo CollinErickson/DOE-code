f66b2df6daa90eed09fb7f49dce5c577ab71d88c
Collin Erickson <collinberickson@gmail.com>
2019-11-21 08:43:04 -0500

    Trying to make quintile grad plots

---
+++ check_hist_by_grad_quintile.R
@@ -0,0 +1,10 @@
+gramacy6D1$outdf %>% head
+tdf <- gramacy6D1$outdf %>% select(n, Group,
+                                   obj, selection_method, design, des_func,
+                                   actual_intsqerrquants.1,
+                                   actual_intsqerrquants.2,
+                                   actual_intsqerrquants.3,
+                                   actual_intsqerrquants.4,
+                                   actual_intsqerrquants.5)
+tdf2 <- tdf %>% reshape2::melt(id.vars=c('n', 'Group', 'obj', 'selection_method', 'design', 'des_func'))
+tdf2 %>% filter(n==120) %>% ggplot(aes(value)) + facet_grid(Group ~ variable) + geom_histogram()
